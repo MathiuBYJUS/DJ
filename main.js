75e714655385f47b460e38ce6c261709fd666fe5
MathiuBYJUS <95060413+MathiuBYJUS@users.noreply.github.com>
2022-10-06 19:46:39 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 
 song = "";
-
+rightWristX=0;
+rightWristY=0;
+leftWristX=0;
+leftWristY=0;
 function preload()
 {
   song=loadSound("Beatles.mp3")  
@@ -13,7 +16,30 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet=
+    ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+
+}
+
+function modelLoaded()
+{
+console.log('poseNetisInitialized');
+}
+
+function gotPoses(results)
+{
+if (results.length>0);
+{
+    rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+
+    leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
   
+}
 }
 
 function draw()
