8a2bb113147c906fe8c29b2855e2615dd63f32ae
MathiuBYJUS <95060413+MathiuBYJUS@users.noreply.github.com>
2022-10-13 19:32:53 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ rightWristX=0;
 rightWristY=0;
 leftWristX=0;
 leftWristY=0;
+scoreLeftWrist=0;
+scoreRighttWrist=0;
 function preload()
 {
   song=loadSound("Beatles.mp3")  
@@ -31,6 +33,10 @@ console.log('poseNetisInitialized');
 function gotPoses(results)
 {
 if (results.length>0);
+
+console.log(results);
+scoreLeftWrist = result[0].pose.keypoints[9].score;
+console.log("scoreLeftWrist = " + scoreLeftWrist);
 {
     rightWristX = results[0].pose.rightWrist.x;
 	rightWristY = results[0].pose.rightWrist.y;
@@ -38,6 +44,7 @@ if (results.length>0);
 
     leftWristX = results[0].pose.leftWrist.x;
 	leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
   
 }
 }
@@ -45,7 +52,19 @@ if (results.length>0);
 function draw()
 {
     image(video,0,0,600,600)
-}
+    fill("#0431B4");
+    stroke("#0431B4");
+
+    if(scoreLeftWrist > 0.2)
+    {
+
+    circle(leftWristX,leftWristY,20);
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    volumen = remove_decimals/500;
+    document.getElementById("volumen").innerHTML = "Volumen=" + volumen;
+    song.setVolume(volumen);
+}}
 
 function play()
 {
